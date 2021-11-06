8e32e96a7b3bfece6b1f1ef088e7f0442114de24
Carlos Eduardo <e.marquesdev@gmail.com>
2021-11-06 18:46:37 -0300

    first commit

---
+++ src/test/java/com/emarques/springmvc/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emarques.springmvc.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
