9d213977d5c3f7dafd6e0c969375b7bbd813dffb
Carlos Eduardo <e.marquesdev@gmail.com>
2021-11-06 23:45:06 -0300

    Aula 03.04: Injeção de dependências

--- src/main/java/com/emarques/springmvc/mudi/controller/HomeController.java
+++ src/main/java/com/emarques/springmvc/mudi/controller/HomeController.java
@@ -1,29 +1,26 @@
 package com.emarques.springmvc.mudi.controller;
 
 import com.emarques.springmvc.mudi.model.Pedido;
+import com.emarques.springmvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    PedidoRepository pedidoRepository;
 
     @GetMapping("/home")
-    public String home(Model model){
+    public String home(Model model) {
 
+        List<Pedido> pedidos = pedidoRepository.recuperaTodosOsPedidos();
 
-        Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-        List<Pedido> pedidos = query.getResultList();
-        model.addAttribute("pedidos",pedidos);
+        model.addAttribute("pedidos", pedidos);
         return "home";
     }
 }
