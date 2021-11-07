e5305e19f25e218654080dde613643b3d8a9d7dc
Carlos Eduardo <e.marquesdev@gmail.com>
2021-11-06 23:52:28 -0300

    Aula 03.06: Usando Spring Data JPA

--- src/main/java/com/emarques/springmvc/mudi/controller/HomeController.java
+++ src/main/java/com/emarques/springmvc/mudi/controller/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
     @GetMapping("/home")
     public String home(Model model) {
 
-        List<Pedido> pedidos = pedidoRepository.recuperaTodosOsPedidos();
+        List<Pedido> pedidos = pedidoRepository.findAll();
 
         model.addAttribute("pedidos", pedidos);
         return "home";
--- src/main/java/com/emarques/springmvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/emarques/springmvc/mudi/repository/PedidoRepository.java
@@ -1,21 +1,9 @@
 package com.emarques.springmvc.mudi.repository;
 
 import com.emarques.springmvc.mudi.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.List;
-
 @Repository
-public class PedidoRepository {
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public List<Pedido> recuperaTodosOsPedidos() {
-        Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-        return query.getResultList();
-    }
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
