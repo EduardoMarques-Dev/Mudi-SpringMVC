c6fb6e7b9660cfaa80f266737caa8b01c593bd7c
e.marquesdev <e.marquesdev@gmail.com>
2021-11-10 14:17:14 -0300

    Aula 01.05: Página de Login

--- src/main/java/com/emarques/springmvc/mudi/WebSecurityConfig.java
+++ src/main/java/com/emarques/springmvc/mudi/WebSecurityConfig.java
@@ -20,7 +20,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			authorizeRequests()
 				.anyRequest().authenticated()
 			.and()
-				.httpBasic();
+				.formLogin(form -> form
+						.loginPage("/login")
+						.permitAll()
+				);
 	}
 
 	@Bean
