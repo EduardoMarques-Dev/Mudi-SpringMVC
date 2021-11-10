ae7fb80accc8c90bbe86bbdd6578957e3c61b80c
e.marquesdev <e.marquesdev@gmail.com>
2021-11-10 14:54:30 -0300

    Aula 01.07: Implementando o Logout

--- src/main/java/com/emarques/springmvc/mudi/WebSecurityConfig.java
+++ src/main/java/com/emarques/springmvc/mudi/WebSecurityConfig.java
@@ -23,7 +23,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.formLogin(form -> form
 						.loginPage("/login")
 						.permitAll()
-				);
+				)
+				.logout(logout -> logout.logoutUrl("/logout")
+						);
 	}
 
 	@Bean
